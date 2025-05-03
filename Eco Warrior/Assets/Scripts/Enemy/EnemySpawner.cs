98fa3e60d8790fff8dcb75cccd0cff2b1e8755aa
Oskar Andreasson <159390304+HissAnd@users.noreply.github.com>
2025-05-03 23:06:19 +0200

    EnemySpawner is working

--- Eco Warrior/Assets/Scripts/Enemy/EnemySpawner.cs
+++ Eco Warrior/Assets/Scripts/Enemy/EnemySpawner.cs
@@ -8,15 +8,17 @@ public class EnemySpawner : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] GameObject enemyPrefab;
-    [SerializeField] public Transform[] WayPoints;
-    private int amountOfEnemies;
-    private int level = 1;
     private Dictionary<int, int> enemySpawnerList = new Dictionary<int, int>
     {
         {1, 2},
-        {2, 10},
+        {2, 5},
         {3, 15},
     };
+    private int level = 2;
+    private float minimumSpawnTime = 1;
+    private float maximumSpawnTime = 4;
+    private float timeToSpawn;
+
     void Start()
     {
         Spawn();
@@ -25,15 +27,13 @@ public class EnemySpawner : MonoBehaviour
     void Spawn()
     {
         int enemiesByLevel = enemySpawnerList.Where(a => a.Key.Equals(level)).Select(b => b.Value).FirstOrDefault();
-        for (int i = 0; i >= enemiesByLevel; i++)
-        {
-            Instantiate(enemyPrefab, WayPoints[i].position, Quaternion.identity);
-        }
+        var spawnPoint = GameObject.FindWithTag("EnemySpawnWaypoint");
+        for (int i = 0; i < enemiesByLevel; i++)
+            Instantiate(enemyPrefab, spawnPoint.transform.position, Quaternion.identity);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
