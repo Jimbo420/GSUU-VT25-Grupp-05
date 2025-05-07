4d3b2cd8e645a3e847ba3efa949f7aca88748390
celinben <160639098+celinben@users.noreply.github.com>
2025-05-07 21:45:03 +0200

    Lagt in logiken för växling av bakgrundsmusik i MusicManager

---
+++ Eco Warrior/Assets/Scripts/Managers/MusicManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MusicManager : MonoBehaviour
+{
+    public static MusicManager Instance;
+
+    [Header("Audio Mixer Setup")]
+    public AudioMixer audioMixer;
+
+    [Header("Audio Source")]
+    public AudioSource musicSource;
+
+    [Header("Music Clips")]
+    public AudioClip calmMusic;
+    public AudioClip tensionMusic;
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void PlayCalmMusic()
+    {
+        musicSource.clip = calmMusic;
+        musicSource.loop = true;
+        musicSource.Play();
+        audioMixer.FindSnapshot("Calm").TransitionTo(1f);
+    }
+
+    public void PlayTensionMusic()
+    {
+        musicSource.clip = tensionMusic;
+        musicSource.loop = true;
+        musicSource.Play();
+        audioMixer.FindSnapshot("Tension").TransitionTo(1f);
+    }
+}
+
+
