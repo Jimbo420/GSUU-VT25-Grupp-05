d03ea1d25893c4382c1c8394ed75d2d7476c0652
Oskar Andreasson <159390304+HissAnd@users.noreply.github.com>
2025-05-07 15:23:52 +0200

    Spawner works perfect

--- Eco Warrior/Assets/Scripts/Enemy/EnemySpawner.cs
+++ Eco Warrior/Assets/Scripts/Enemy/EnemySpawner.cs
@@ -8,18 +8,25 @@ using UnityEngine.AI;
 
 public class EnemySpawner : MonoBehaviour
 {
-    [SerializeField] GameObject enemyPrefab;
-    private Dictionary<int, int> enemySpawnerList = new Dictionary<int, int>
+    [SerializeField] GameObject _enemyPrefabPistol;
+    [SerializeField] GameObject _enemyPrefabSMG;
+    private Dictionary<int, int> _enemySpawnerList = new Dictionary<int, int>
     {
-        {1, 2},
+        {1, 3},
         {2, 5},
-        {3, 15},
+        {3, 7},
     };
-    private int level = 1;
+    private int level;
+
+    private void Awake()
+    {
+        level = 2;
+        StartCoroutine(Spawn());
+
+    }
 
     void Start()
     {
-        StartCoroutine(Spawn());
     }
 
     void Update()
@@ -28,11 +35,21 @@ public class EnemySpawner : MonoBehaviour
 
     IEnumerator Spawn()
     {
-        int enemiesByLevel = enemySpawnerList.Where(a => a.Key == level).Select(b => b.Value).FirstOrDefault();
+        int enemiesToSpawn = 0;
+
+        if (_enemySpawnerList.ContainsKey(level))
+            enemiesToSpawn = _enemySpawnerList[level];
+
         var spawnPoint = GameObject.FindWithTag("EnemySpawnWaypoint");
-        for (int i = 0; i < enemiesByLevel; i++)
+
+        for (int i = 0; i < enemiesToSpawn; i++)
         {
-            Instantiate(enemyPrefab, spawnPoint.transform.position, Quaternion.identity);
+            GameObject enemyToSpawn = _enemyPrefabPistol;
+
+            if (i < level)
+                enemyToSpawn = _enemyPrefabSMG;
+
+            Instantiate(enemyToSpawn, spawnPoint.transform.position, Quaternion.identity);
             yield return new WaitForSeconds(5f);
         }
     }
