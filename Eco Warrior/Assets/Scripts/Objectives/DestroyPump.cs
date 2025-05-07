45bd3b3ada4ffffac46bccb327fff9e87fa533ee
NoelStark <noel.k.stark@gmail.com>
2025-05-07 12:47:55 +0200

    Animated Objectives & Implemented Ability to Destroy Power Supply

---
+++ Eco Warrior/Assets/Scripts/Objectives/DestroyPump.cs
@@ -0,0 +1,49 @@
+using Assets.Scripts.Objectives;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DestroyPump : MonoBehaviour, IDestroy
+{
+    public GameObject textObject;
+    [SerializeField] private Sprite _destroyedSprite;
+    private Slider _slider;
+    private bool _isInRange;
+    private bool _isDestroyed;
+
+    void Awake()
+    {
+        _slider = GetComponentInChildren<Slider>();
+    }
+
+    void FixedUpdate()
+    {
+        if (!_isInRange) return;
+        if (!Input.GetKey(KeyCode.F)) return;
+
+        _slider.value -= 0.1f;
+
+        if (_slider.value == 0)
+            Destroy();
+        
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (_isDestroyed) return;
+        textObject.SetActive(true);
+        _isInRange = true;
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        textObject.SetActive(false);
+        _isInRange = false;
+    }
+
+    public void Destroy()
+    {
+        _slider.gameObject.SetActive(false);
+        textObject.gameObject.SetActive(false);
+        GetComponent<Animator>().enabled = false;
+        GetComponent<SpriteRenderer>().sprite = _destroyedSprite;
+        _isDestroyed = true;
+    }
+}
