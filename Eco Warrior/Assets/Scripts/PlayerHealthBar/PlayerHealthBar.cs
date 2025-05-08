3567406865acd38fc789cbb809a5e8390709f59f
Oskar Andreasson <159390304+HissAnd@users.noreply.github.com>
2025-05-08 20:20:30 +0200

    Heartbar is fixed and script in progress

--- Eco Warrior/Assets/Scripts/PlayerHealthBar/PlayerHealthBar.cs
+++ Eco Warrior/Assets/Scripts/PlayerHealthBar/PlayerHealthBar.cs
@@ -6,33 +6,42 @@ public class PlayerHealthBar : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
-    [SerializeField] Sprite _fullHeart;
-    [SerializeField] Sprite _halfHeart;
-    [SerializeField] Sprite _emptyHeart;
-    [SerializeField] private List<Sprite> heartImages;
+    public Sprite _fullHeart;
+    public Sprite _halfHeart;
+    public Sprite _emptyHeart;
+    private Sprite _heartImage;
     //private List<int, int>();
     void Start()
     {
-        
+        _heartImage = GetComponent<Sprite>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        UpdatePlayersHearts(10);
-        Debug.Log("Hej");
     }
 
-    public void UpdatePlayersHearts(int health)
+    public void SetHeartHealth(Heart heart)
     {
-        for (int i = 0; i < heartImages.Count; i++)
+        switch(heart)
         {
-            if (health >= (i + 1) * 2)
-                heartImages[i] = _fullHeart;
-            else if (health == (i * 2) + 1)
-                heartImages[i] = _halfHeart;
-            else
-                heartImages[i] = _emptyHeart;
+            case Heart.Full:
+                _heartImage = _fullHeart;
+                break;
+            case Heart.Half:
+                _heartImage = _halfHeart;
+                break;
+            case Heart.Empty:
+                _heartImage = _emptyHeart;
+                break;
+
         }
+    }
+
+    public enum Heart
+    {
+        Full = 2,
+        Half = 1,
+        Empty = 0
     }
 }
