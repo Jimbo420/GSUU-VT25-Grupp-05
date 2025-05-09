2abde5102b4f2d45839dd7d908b16e9c6dff51f0
Oskar Andreasson <159390304+HissAnd@users.noreply.github.com>
2025-05-09 08:20:51 +0200

    Enemy health haerts updated and level based on spawn

--- Eco Warrior/Assets/Scripts/MainCharacter/PlayerHealth.cs
+++ Eco Warrior/Assets/Scripts/MainCharacter/PlayerHealth.cs
@@ -3,11 +3,11 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public float _health = 25;
-    public float _maxHealth = 25;
+    private float _health = 10;
+    private float _maxHealth = 10;
     [SerializeField] private PlayerHealthBarParent _healthBarParent;
 
-    void Start()
+    void Awake()
     {
         _health = _maxHealth;
         _healthBarParent.UpdateHearts(_health);
@@ -20,32 +20,18 @@ public class PlayerHealth : MonoBehaviour
         
     }
 
-    public void Health(float health, float maxhealth)
-    {
-        //Slider value by enemies _health
-        //foreach (var image in slider.GetComponentsInChildren<Image>())
-        //    image.enabled = health != maxhealth;
-
-        //slider.maxValue = maxhealth;
-        //slider.value = health;
-    }
     public void Heal()
     {
         _health = _maxHealth;
         _healthBarParent.UpdateHearts(_health);
-        Health(_health, _maxHealth);
     }
     public void HitDamage(float damage, GameObject entity)
     {
         _health -= damage;
         _health = Mathf.Clamp(_health, 0, _maxHealth);
         _healthBarParent.UpdateHearts(_health);
-        Health(_health, _maxHealth);
         if (_health <= 0)
             Destroy(entity);
-        //else
-            //_healthBarParent.UpdateHearts(_health);
-
         Debug.Log("Spelarens Health: " + _health);
     }
     public void Dead()
--- Eco Warrior/Assets/Scripts/PlayerHealthBar/PlayerHealthBarParent.cs
+++ Eco Warrior/Assets/Scripts/PlayerHealthBar/PlayerHealthBarParent.cs
@@ -13,31 +13,15 @@ public class PlayerHealthBarParent : MonoBehaviour
     public Sprite _fullHeart;
     public Sprite _halfHeart;
     public Sprite _emptyHeart;
-
-    public float maxHealth = 10f;
-    public float currentHealth = 10f;
-    //private void Start()
-    //{
-    //    //InstantiateHearts();
-    //    GameObject[] heartObjects = GameObject.FindGameObjectsWithTag("PlayerHearts");
-    //    _hearts = new Image[heartObjects.Length];
-
-    //    for (int i = 0; i < heartObjects.Length; i++)
-    //    {
-    //        _hearts[i] = heartObjects[i].GetComponent<Image>();
-    //    }
-
-    //}
     private void Awake()
     {
         Transform[] children = GetComponentsInChildren<Transform>();
         List<Image> foundHearts = new List<Image>();
-
         foreach (Transform child in children)
         {
-            Image img = child.GetComponent<Image>();
-            if (img != null)
-                foundHearts.Add(img);
+            Image heart = child.GetComponent<Image>();
+            if (heart != null)
+                foundHearts.Add(heart);
         }
 
         _hearts = foundHearts.ToArray();
@@ -45,64 +29,19 @@ public class PlayerHealthBarParent : MonoBehaviour
 
     public void UpdateHearts(float currentHealth)
     {
-        if (_hearts == null || _hearts.Length == 0)
-        {
-            Debug.LogError("Hearts array is not initialized!");
-            return;
-        }
-
+        Debug.Log("Tja");
         for (int i = 0; i < _hearts.Length; i++)
         {
-            if (_hearts[i] == null) continue; // skydda vid tomma entries
+            //if (_hearts[i] == null) continue;
 
             float heartHealth = currentHealth - (i * 2);
-
+            Debug.Log("HeartHealth " + heartHealth);
             if (heartHealth >= 2)
                 _hearts[i].sprite = _fullHeart;
-            else if (heartHealth == 1)
+            else if (heartHealth.Equals(1))
                 _hearts[i].sprite = _halfHeart;
             else
                 _hearts[i].sprite = _emptyHeart;
         }
     }
-    //public void InstantiateHearts()
-    //{
-    //    ClearHearts();
-    //    float maxHealthRem = _playerHealth._maxHealth % 2;
-    //    int heartsToMake = (int)((_playerHealth._maxHealth / 5) + maxHealthRem);
-    //    for (int i = 0; i < heartsToMake; i++) 
-    //        CreateEmptyHeart();
-
-    //    for (int i = 0;i < _heartList.Count; i++)
-    //    {
-    //        int heartStatusRem = (int)Mathf.Clamp(_playerHealth._health - (i*2), 0, 2);
-    //        _heartList[i].SetHeartHealth((Heart)heartStatusRem);
-    //    }
-
-    //}
-    //public void CreateEmptyHeart()
-    //{
-    //    GameObject newHeart = Instantiate(_healthBarPrefab);
-    //    newHeart.transform.SetParent(transform);
-    //    PlayerHealthBar heartComponent = newHeart.GetComponent<PlayerHealthBar>();
-    //    heartComponent.SetHeartHealth(Heart.Empty);
-    //    _heartList.Add(heartComponent);
-    //}
-    //public void ClearHearts()
-    //{
-    //    for (int i = transform.childCount - 1; i >= 0; i--)
-    //    {
-    //        DestroyImmediate(transform.GetChild(i).gameObject);
-    //    }
-    //    _heartList.Clear();
-    //}
-
-    //public void UpdateHearts(float currentHealth)
-    //{
-    //    for (int i = 0; i < _heartList.Count; i++)
-    //    {
-    //        int heartValue = Mathf.Clamp((int)(currentHealth - (i * 2)), 0, 2);
-    //        _heartList[i].SetHeartHealth((Heart)heartValue);
-    //    }
-    //}
 }
